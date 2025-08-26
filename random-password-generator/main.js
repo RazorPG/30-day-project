3ff720ee576733a0bb2c00e64868a9bf172ef23b
RazorPG <rafidhilmi0504@gmail.com>
2025-08-26 15:45:50 +0700

    project-day4-random-password-generator

---
+++ random-password-generator/main.js
@@ -0,0 +1,25 @@
+// query selector dom
+const generateBtn = document.querySelector('button')
+const generatePassword = document.querySelector('input')
+const copyPassword = document.querySelector('img')
+
+// button generate password
+generateBtn.addEventListener('click', () => {
+  let result = ''
+  for (let i = 0; i < 16; i++) {
+    result += String.fromCharCode(Math.floor(Math.random() * 65 + 60))
+  }
+  generatePassword.value = result
+})
+
+// button copy password
+copyPassword.addEventListener('click', () => {
+  navigator.clipboard
+    .writeText(generatePassword.value)
+    .then(() => {
+      alert('Password has been copied!')
+    })
+    .catch(err => {
+      alert('Failed to copy password')
+    })
+})
