43236861746f106609ebcca14b340ab91a1b4066
RazorPG <rafidhilmi0504@gmail.com>
2025-09-09 09:49:03 +0700

    project-day-18-email-subcription-form

---
+++ email-subscription-form/main.js
@@ -0,0 +1,16 @@
+// api ke google sheets
+const scriptURL =
+  'https://script.google.com/macros/s/AKfycbyy0pD2dVUgzz-6XOtLwK3TK4Iqe7IfDMeEBhwKAe5CWaLxa1fvbiadjmNDkiodswRa/exec'
+
+// mengambil query form
+const form = document.forms['submit-to-google-sheet']
+
+// add event listener form submit
+form.addEventListener('submit', e => {
+  // tidak reset ketika submit
+  e.preventDefault()
+  // fetch api lalu post ke google sheets
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})
